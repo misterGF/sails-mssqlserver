aa703ebebf6a6c6a844904089869c58507d601d1
Travis Webb <me@traviswebb.com>
2015-01-17 02:23:51 -0500

    all semantic tests passing except for UPDATE
    modified:   lib/adapter.js
    - drop if exists
    modified:   lib/sql.js
    - updated type casting
    modified:   test/integration/runner.js
    - clear database before running tests

--- test/integration/runner.js
+++ test/integration/runner.js
@@ -38,6 +38,89 @@ log('Latest draft of Waterline adapter interface spec:');
 log('http://links.sailsjs.org/docs/plugins/adapters/interfaces');
 console.log();
 
+var mssql = require('mssql');
+
+console.log('Preparing database...');
+var connection = new mssql.Connection({
+    user: process.env.MSSQL_USER,
+    password: process.env.MSSQL_PASSWORD,
+    server: process.env.MSSQL_HOST,
+    database: process.env.MSSQL_DATABASE,
+    options: {
+      encrypt: true
+    }
+  }, function (err) {
+    new mssql.Request(connection).query([
+      'IF OBJECT_ID(\'dbo.apartmentTable\', \'U\') IS NOT NULL DROP TABLE apartmentTable',
+      'IF OBJECT_ID(\'dbo.customer_manyTable\', \'U\') IS NOT NULL DROP TABLE customer_manyTable',
+      'IF OBJECT_ID(\'dbo.customerbelongsTable\', \'U\') IS NOT NULL DROP TABLE customerbelongsTable',
+      'IF OBJECT_ID(\'dbo.customerTable\', \'U\') IS NOT NULL DROP TABLE customerTable',
+      'IF OBJECT_ID(\'dbo.driver_taxis_taxi_drivers\', \'U\') IS NOT NULL DROP TABLE driver_taxis_taxi_drivers',
+      'IF OBJECT_ID(\'dbo.driverTable\', \'U\') IS NOT NULL DROP TABLE driverTable',
+      'IF OBJECT_ID(\'dbo.payment_manyTable\', \'U\') IS NOT NULL DROP TABLE payment_manyTable',
+      'IF OBJECT_ID(\'dbo.paymentbelongsTable\', \'U\') IS NOT NULL DROP TABLE paymentbelongsTable',
+      'IF OBJECT_ID(\'dbo.paymentTable\', \'U\') IS NOT NULL DROP TABLE paymentTable',
+      'IF OBJECT_ID(\'dbo.profileTable\', \'U\') IS NOT NULL DROP TABLE profileTable',
+      'IF OBJECT_ID(\'dbo.stadiumTable\', \'U\') IS NOT NULL DROP TABLE stadiumTable',
+      'IF OBJECT_ID(\'dbo.taxiTable\', \'U\') IS NOT NULL DROP TABLE taxiTable',
+      'IF OBJECT_ID(\'dbo.teamTable\', \'U\') IS NOT NULL DROP TABLE teamTable',
+      'IF OBJECT_ID(\'dbo.user_resourceTable\', \'U\') IS NOT NULL DROP TABLE user_resourceTable',
+      'IF OBJECT_ID(\'dbo.userTable\', \'U\') IS NOT NULL DROP TABLE userTable',
+      'IF OBJECT_ID(\'dbo.userTable2\', \'U\') IS NOT NULL DROP TABLE userTable2',
+      'IF OBJECT_ID(\'dbo.venueTable\', \'U\') IS NOT NULL DROP TABLE venueTable'
+    ].join(';'), function (err, results) {
+      if (err) throw err;
+
+      console.log('Running tests...');
+      
+      new TestRunner({
+
+        mocha: {
+          reporter: 'spec',
+          timeout: 120000
+        },
+
+        // Load the adapter module.
+        adapter: Adapter,
+
+        // ADD YOUR CONFIG HERE
+        config: {
+          timeout: 120000,
+          pool: {
+            min: 1,
+            max: 1,
+            idleTimeout: 120000
+          },
+          migrate: 'drop'
+        },
+
+        failOnError: true,
+
+        // The set of adapter interfaces to test against.
+        // (grabbed these from this adapter's package.json file above)
+        interfaces: interfaces
+
+        // Most databases implement 'semantic' and 'queryable'.
+        //
+        // As of Sails/Waterline v0.10, the 'associations' interface
+        // is also available.  If you don't implement 'associations',
+        // it will be polyfilled for you by Waterline core.  The core
+        // implementation will always be used for cross-adapter / cross-connection
+        // joins.
+        //
+        // In future versions of Sails/Waterline, 'queryable' may be also
+        // be polyfilled by core.
+        //
+        // These polyfilled implementations can usually be further optimized at the
+        // adapter level, since most databases provide optimizations for internal
+        // operations.
+        //
+        // Full interface reference:
+        // https://github.com/balderdashy/sails-docs/blob/master/adapter-specification.md
+      });
+    });
+  });
+
 /**
  * Integration Test Runner
  *
@@ -45,47 +128,3 @@ console.log();
  * run mocha tests against the specified interfaces
  * of the currently-implemented Waterline adapter API.
  */
-new TestRunner({
-
-  mocha: {
-    reporter: 'spec',
-    timeout: 120000
-  },
-
-  // Load the adapter module.
-  adapter: Adapter,
-
-  // ADD YOUR CONFIG HERE
-  config: {
-    timeout: 120000,
-    pool: {
-      min: 1,
-      max: 10,
-      idleTimeout: 60000
-    }
-  },
-
-  failOnError: true,
-
-  // The set of adapter interfaces to test against.
-  // (grabbed these from this adapter's package.json file above)
-  interfaces: interfaces
-
-  // Most databases implement 'semantic' and 'queryable'.
-  //
-  // As of Sails/Waterline v0.10, the 'associations' interface
-  // is also available.  If you don't implement 'associations',
-  // it will be polyfilled for you by Waterline core.  The core
-  // implementation will always be used for cross-adapter / cross-connection
-  // joins.
-  //
-  // In future versions of Sails/Waterline, 'queryable' may be also
-  // be polyfilled by core.
-  //
-  // These polyfilled implementations can usually be further optimized at the
-  // adapter level, since most databases provide optimizations for internal
-  // operations.
-  //
-  // Full interface reference:
-  // https://github.com/balderdashy/sails-docs/blob/master/adapter-specification.md
-});
