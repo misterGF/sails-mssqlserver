e5157d8baf0878f11cf8012d2a7fcc51d8c5423c
Travis Webb <me@traviswebb.com>
2015-01-19 14:35:23 -0500

    use azure-compatible table drop query

--- test/integration/runner.js
+++ test/integration/runner.js
@@ -51,7 +51,25 @@ var connection = new mssql.Connection({
     }
   }, function (err) {
     if (err) throw err;
-    new mssql.Request(connection).query('EXEC sp_MSForEachTable \'DROP TABLE ?\'', function (err, results) {
+    new mssql.Request(connection).query([
+      'while(exists(select 1 from INFORMATION_SCHEMA.TABLE_CONSTRAINTS where CONSTRAINT_TYPE=\'FOREIGN KEY\'))',
+      'begin',
+        'declare @sql nvarchar(2000)',
+        'SELECT TOP 1 @sql=(\'ALTER TABLE \' + TABLE_SCHEMA + \'.[\' + TABLE_NAME + \'] DROP CONSTRAINT [\' + CONSTRAINT_NAME + \']\')',
+        'FROM information_schema.table_constraints',
+        'WHERE CONSTRAINT_TYPE = \'FOREIGN KEY\'',
+        'exec (@sql)',
+        'PRINT @sql',
+      'end;',
+      'while(exists(select 1 from INFORMATION_SCHEMA.TABLES where TABLE_NAME != \'__MigrationHistory\'))',
+      'begin',
+        'SELECT TOP 1 @sql=(\'DROP TABLE \' + TABLE_SCHEMA + \'.[\' + TABLE_NAME + \']\')',
+        'FROM INFORMATION_SCHEMA.TABLES',
+        'WHERE TABLE_NAME != \'__MigrationHistory\'',
+        'exec (@sql)',
+        'PRINT @sql',
+      'end'
+    ].join(' '), function (err, results) {
       if (err) throw err;
 
       console.log('Running tests...');
