036942396c1405fdb96527400299a022d4b8c492
Gil Ferreira <gil.ferreira@mindshift.com>
2016-06-21 09:55:18 -0400

    Big update. Changed coding style to standard. Added some error checking. Fixed runner for testing

--- test/integration/runner.js
+++ test/integration/runner.js
@@ -10,21 +10,20 @@
 
 var util = require('util');
 var mocha = require('mocha');
-var log = new (require('captains-log'))();
+var log = new(require('captains-log'))();
 var TestRunner = require('waterline-adapter-tests');
 var Adapter = require('../../lib/adapter.js');
-
+require('dotenv').config();
 // Grab targeted interfaces from this adapter's `package.json` file:
 var package = {};
 var interfaces = [];
 try {
   package = require('../../package.json');
   interfaces = package.waterlineAdapter.interfaces;
-}
-catch (e) {
+} catch(e) {
   throw new Error(
-    '\n'+
-    'Could not read supported interfaces from `waterlineAdapter.interfaces`'+'\n' +
+    '\n' +
+    'Could not read supported interfaces from `waterlineAdapter.interfaces`' + '\n' +
     'in this adapter\'s `package.json` file ::' + '\n' +
     util.inspect(e)
   );
@@ -42,85 +41,88 @@ var mssql = require('mssql');
 
 console.log('Dropping any existing tables...');
 var connection = new mssql.Connection({
-    user: process.env.MSSQL_USER,
-    password: process.env.MSSQL_PASSWORD,
-    server: process.env.MSSQL_HOST,
-    database: process.env.MSSQL_DATABASE,
-  }, function (err) {
-    if(err && err.name.indexOf('ConnectionError') == 0) {
-      console.log('Connection to DB not implemented yet.')
-      return
-    }
-    else if (err) {
-      throw err
-    }
+  user: process.env.MSSQL_USER,
+  password: process.env.MSSQL_PASSWORD,
+  server: process.env.MSSQL_HOST,
+  database: process.env.MSSQL_DATABASE,
+}, function (err) {
+  if(err && err.name.indexOf('ConnectionError') == 0) {
+    console.log('Connection to DB not implemented yet.', err)
+    return
+  } else if(err) {
+    throw err
+  }
 
-    new mssql.Request(connection).query([
-      'while(exists(select 1 from INFORMATION_SCHEMA.TABLE_CONSTRAINTS where CONSTRAINT_TYPE=\'FOREIGN KEY\'))',
-      'begin',
-        'declare @sql nvarchar(2000)',
-        'SELECT TOP 1 @sql=(\'ALTER TABLE \' + TABLE_SCHEMA + \'.[\' + TABLE_NAME + \'] DROP CONSTRAINT [\' + CONSTRAINT_NAME + \']\')',
-        'FROM information_schema.table_constraints',
-        'WHERE CONSTRAINT_TYPE = \'FOREIGN KEY\'',
-        'exec (@sql)',
-        'PRINT @sql',
-      'end;',
-      'while(exists(select 1 from INFORMATION_SCHEMA.TABLES where TABLE_NAME != \'__MigrationHistory\'))',
-      'begin',
-        'SELECT TOP 1 @sql=(\'DROP TABLE \' + TABLE_SCHEMA + \'.[\' + TABLE_NAME + \']\')',
-        'FROM INFORMATION_SCHEMA.TABLES',
-        'WHERE TABLE_NAME != \'__MigrationHistory\'',
-        'exec (@sql)',
-        'PRINT @sql',
-      'end'
-    ].join(' '), function (err, results) {
-      if (err) throw err;
-      console.log('Starting test runner...');
+  new mssql.Request(connection).query([
+    'while(exists(select 1 from INFORMATION_SCHEMA.TABLE_CONSTRAINTS where CONSTRAINT_TYPE=\'FOREIGN KEY\'))',
+    'begin',
+    'declare @sql nvarchar(2000)',
+    'SELECT TOP 1 @sql=(\'ALTER TABLE \' + TABLE_SCHEMA + \'.[\' + TABLE_NAME + \'] DROP CONSTRAINT [\' + CONSTRAINT_NAME + \']\')',
+    'FROM information_schema.table_constraints',
+    'WHERE CONSTRAINT_TYPE = \'FOREIGN KEY\'',
+    'exec (@sql)',
+    'PRINT @sql',
+    'end;',
+    'while(exists(select 1 from INFORMATION_SCHEMA.TABLES where TABLE_NAME != \'__MigrationHistory\'))',
+    'begin',
+    'SELECT TOP 1 @sql=(\'DROP TABLE \' + TABLE_SCHEMA + \'.[\' + TABLE_NAME + \']\')',
+    'FROM INFORMATION_SCHEMA.TABLES',
+    'WHERE TABLE_NAME != \'__MigrationHistory\'',
+    'exec (@sql)',
+    'PRINT @sql',
+    'end'
+  ].join(' '), function (err, results) {
+    if(err) throw err;
+    console.log('Starting test runner...');
 
-      new TestRunner({
+    new TestRunner({
 
-        mocha: {
-          reporter: 'spec',
-          timeout: 300 * 1000
+      mocha: {
+        reporter: 'spec',
+        timeout: 300 * 1000
+      },
+
+      // Load the adapter module.
+      adapter: Adapter,
+
+      // ADD YOUR CONFIG HERE
+      config: {
+        timeout: 300 * 1000,
+        pool: {
+          max: 100
         },
+        user: process.env.MSSQL_USER,
+        password: process.env.MSSQL_PASSWORD,
+        server: process.env.MSSQL_HOST,
+        database: process.env.MSSQL_DATABASE
+      },
 
-        // Load the adapter module.
-        adapter: Adapter,
+      failOnError: true,
 
-        // ADD YOUR CONFIG HERE
-        config: {
-          timeout: 300 * 1000,
-          pool: {
-            max: 100
-          }
-        },
+      // The set of adapter interfaces to test against.
+      // (grabbed these from this adapter's package.json file above)
+      interfaces: interfaces
 
-        failOnError: true,
-
-        // The set of adapter interfaces to test against.
-        // (grabbed these from this adapter's package.json file above)
-        interfaces: interfaces
-
-        // Most databases implement 'semantic' and 'queryable'.
-        //
-        // As of Sails/Waterline v0.10, the 'associations' interface
-        // is also available.  If you don't implement 'associations',
-        // it will be polyfilled for you by Waterline core.  The core
-        // implementation will always be used for cross-adapter / cross-connection
-        // joins.
-        //
-        // In future versions of Sails/Waterline, 'queryable' may be also
-        // be polyfilled by core.
-        //
-        // These polyfilled implementations can usually be further optimized at the
-        // adapter level, since most databases provide optimizations for internal
-        // operations.
-        //
-        // Full interface reference:
-        // https://github.com/balderdashy/sails-docs/blob/master/adapter-specification.md
-      });
+      // Most databases implement 'semantic' and 'queryable'.
+      //
+      // As of Sails/Waterline v0.10, the 'associations' interface
+      // is also available.  If you don't implement 'associations',
+      // it will be polyfilled for you by Waterline core.  The core
+      // implementation will always be used for cross-adapter / cross-connection
+      // joins.
+      //
+      // In future versions of Sails/Waterline, 'queryable' may be also
+      // be polyfilled by core.
+      //
+      // These polyfilled implementations can usually be further optimized at the
+      // adapter level, since most databases provide optimizations for internal
+      // operations.
+      //
+      // Full interface reference:
+      // https://github.com/balderdashy/sails-docs/blob/master/adapter-specification.md
     });
   });
+});
 
 /**
  * Integration Test Runner
